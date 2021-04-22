2107b71f68a679061ed920d93b606614658de978
apurva1635 <82993981+apurva1635@users.noreply.github.com>
2021-04-23 01:23:23 +1000

    Import Error Resolved.
    color changed to java.awt.Color from JFrame library.

--- AK_GameBoard.java
+++ AK_GameBoard.java
@@ -7,10 +7,10 @@ public class AK_GameBoard extends JPanel {
     int xcells = 30;    
     int ycells = 30;
     int szcell = 10;        
-       
+      
     public AK_GameBoard() {
         setPreferredSize(new Dimension(xcells * szcell, ycells * szcell));        
-        setBackground(color.black);
+        setBackground(Color.black);
         setFocusable(true);        
     }
 
